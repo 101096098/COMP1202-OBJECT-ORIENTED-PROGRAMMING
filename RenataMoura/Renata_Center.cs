996948196294eaf708b4e99dae29fdaaa421f96c
Renata Moura <101096098@georgebrown.ca>
2019-03-13 11:19:35 -0400

    FinalProject

---
+++ RenataMoura/Renata_Center.cs
@@ -0,0 +1,141 @@
+﻿/*
+ *Comments: - Assignment C#     Summer 2017.
+ * Author: Renata Moura  - Student ID: 101096098
+ * This class defines Centers characteristsc including
+ * Name, city, price for adult tickets, number of adutls
+ * and children visits.
+ */
+
+ 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RenataMoura
+{
+    class Renata_Center
+    {
+        // variables definition:
+
+        private string centerName; // Center Name
+        private string centerCity; // Center City
+        private double ticketPrice; // Price for each month
+
+        private int[] numberOfAdults; // Number of adults per month
+        private int[] numberOfChildren; // Number of Children per month
+
+        // Default constructor
+
+        public Renata_Center()
+        {
+            numberOfAdults = new int[12];
+            numberOfChildren = new int[12];
+        }
+
+
+        // Constructors
+
+        // Constructor for Center Name, Center City, Center Price, Number of Adults and Number of children
+        public Renata_Center(string CenterName, string CenterCity, double TicketPrice, int[] numberOfAdults, int[] numberOfChildren)
+        {
+            centerCity = CenterCity;
+            centerName = CenterName;
+            ticketPrice = TicketPrice;
+            this.numberOfAdults = numberOfAdults;
+            this.numberOfChildren = numberOfChildren;
+
+        }
+
+        // Properties
+        public int[] NumberOfAdults
+        {
+            get { return numberOfAdults; }
+            set { numberOfAdults = value; }
+        }
+
+        public int[] NumberOfChildren
+        {
+            get { return numberOfChildren; }
+            set { numberOfChildren = value; }
+        }
+
+        public string CenterName
+        {
+            get { return centerName; }
+            set { centerName = value; }
+        }
+
+        public string CenterCity
+        {
+            get { return centerCity; }
+            set { centerCity = value; }
+        }
+
+        public double TicketPrice
+        {
+            get { return ticketPrice; }
+            set { ticketPrice = value; }
+        }
+
+        //- statistics showing the month and year for the:
+        //- highest one-day visits by adults
+        public string MaxMonth(int[] tempmonth)
+        {
+            int maxIndex = 0;
+            for (int i = 0; i < tempmonth.Length; i++)
+                if (tempmonth[i] > tempmonth[maxIndex])
+                    maxIndex = i;
+            return Renata_CenterApp.Month[maxIndex];
+        }
+        //- statistics showing the month and year for the:
+        //- highest one-day visits by children
+
+        public string MinMonth(int[] tempmonth)
+        {
+            int minIndex = 0;
+            for (int i = 0; i < 12; i++)
+                if (numberOfAdults[i] != 0)
+                {
+                    minIndex = i;
+                    break;
+                }
+            for (int i = 0; i < tempmonth.Length; i++)
+                if (tempmonth[i] < tempmonth[minIndex] && numberOfAdults[i] != 0)
+                    minIndex = i;
+            return Renata_CenterApp.Month[minIndex];
+        }
+
+        public void Display()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("----------------------------------------------------------------------");
+            Console.ResetColor();
+
+            Console.WriteLine("\nDisplaying Data for Center {0}:", centerName);
+            Console.WriteLine("\nYear 2016\n");
+            for (int i = 0; i < 12; i++)
+            {
+                if (NumberOfAdults[i] != 0 && numberOfChildren[i] != 0)
+                {
+                    Console.WriteLine("Month: {0}", Renata_CenterApp.Month[i]);
+                    Console.WriteLine("\tNumber of Adults: {0}", NumberOfAdults[i]);
+                    Console.WriteLine("\tNumber of Children: {0}", NumberOfChildren[i]);
+                    Console.WriteLine("\tSales for {0} : {1:C2}", Renata_CenterApp.Month[i],
+                        ticketPrice * NumberOfAdults[i]); // total sales for this month 
+                }
+            }
+            Console.WriteLine("\nDisplaying Statistic Report for {0}", centerName);
+            Console.WriteLine("\tHighest one-day visit by adults: {0}", MaxMonth(NumberOfAdults));
+            Console.WriteLine("\tHighest one-day visit by childten: {0}", MaxMonth(NumberOfChildren));
+            Console.WriteLine("\tMonth of Least Sale: {0}", MinMonth(NumberOfAdults));
+            Console.WriteLine("\tAmount of Sale in {0}: {1:C2}", MinMonth(NumberOfAdults),
+                numberOfAdults[Array.IndexOf(Renata_CenterApp.Month, MinMonth(numberOfAdults))]
+                * ticketPrice);  // - least total revenue collected along with the amount. 
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("----------------------------------------------------------------------");
+            Console.ResetColor();
+        }
+    }
+}
---
+++ RenataMoura/Renata_CenterApp.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RenataMoura
+{
+    class Renata_CenterApp
+    {
+       
+        static void Main(string [] args)
+        {            
+            Renata_Center[] centerArray = new Renata_Center[50];
+            centerArray = InputData();
+            Display(centerArray);
+       
+            Console.ReadKey();
+        }
+        public static string[] Month = new string[] { "January", "February", "March", "April", "May",
+            "June", "July","August","Septembter","Octuber","November","December"};
+
+        public static void Display(Renata_Center[] theArray)
+        {
+            for(int i = 0; i<5;i++)
+            {
+                if(theArray[i] != null)
+                {
+                    theArray[i].Display();
+                    Console.WriteLine();
+                }
+            }
+        }
+
+       public static Renata_Center[] InputData() // method
+        {
+            int numMonth, numcenter;
+            int numOfAdult, numOfChild;
+            double ticketPrice;
+            Renata_Center [] tempObject = new Renata_Center[50]; // object
+           
+            // Informations for each center
+            while (true)
+            {
+                
+                Console.WriteLine("\nCenter Statistic study (Renata Moura analisys");
+                Console.WriteLine("\nHow many centres would you like to enter and view statistical data?");
+                if (!int.TryParse(Console.ReadLine(), out numcenter))
+                {
+                    Console.WriteLine("Invalid data entered. Please enter number of center:");
+                    continue;
+                }
+                break;
+            }
+
+            for (int i = 0; i < numcenter; i++)
+            {
+                tempObject[i] = new Renata_Center();
+                while (true)
+                {
+                    Console.WriteLine("Enter Centre Name"); // Input center name
+                    string centerName = Console.ReadLine();
+                    if (centerName == "")// center name validation( Data entered by the user must be validated:
+                        //name of the centre cannot be blank. )
+                    {
+                        Console.WriteLine("Invalid data entered. Please enter a valid name:");
+                        continue;
+                    }
+                    tempObject[i].CenterName = centerName;
+                    break;
+                }
+                // Input Center City
+                Console.WriteLine("Enter Centre City:");
+                string centerCity = Console.ReadLine();
+                tempObject[i].CenterCity = centerCity;
+
+                // Input Ticket price. Data entered by the user must be validated:
+               //- price for adult visitor cannot be less than $20.00 and more than $40.00. 
+                while (true)
+                {
+                    Console.WriteLine("Input Ticket price per day(Adult):");
+                    if (double.TryParse(Console.ReadLine(), out ticketPrice) && ticketPrice >= 20 && ticketPrice <= 40)
+                    {
+                        tempObject[i].TicketPrice = ticketPrice;
+                        break;
+                    }
+                    Console.WriteLine("Invalid Price entered. Please enter a valid price:");
+                }
+
+                // Input number of months and data for months. Data entered by the user must be validated:
+                //- month must be an integer and valid (i.e 1 to 12)
+                while (true)
+                {
+                    while (true)
+                    {
+                        Console.WriteLine("Please enter number of the month:");
+                        if (!int.TryParse(Console.ReadLine(), out numMonth) || numMonth < 0 || numMonth > 12)
+                        {
+                            Console.Write("Invalid Month. Please re-enter:");
+                            continue;
+                        }
+                        break;
+                    }
+
+                    // Input number of adults. Data entered by the user must be validated:
+                    //-visitors(adults/children) can be zero or more (i.e. it cannot be negative)
+
+                    while (true)
+                    {
+                        Console.WriteLine("Please enter number of Adults:");
+
+                        if (int.TryParse(Console.ReadLine(), out numOfAdult) && numOfAdult >= 0)
+                        {
+
+                            tempObject[i].NumberOfAdults[numMonth - 1] = numOfAdult;
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Please enter a valid value.");
+                        }
+                    }
+
+                    // Input number of childrenData entered by the user must be validated:
+                    //-visitors(adults/children) can be zero or more (i.e. it cannot be negative)
+
+                    while (true)
+                    {
+                        Console.WriteLine("Please enter number of Children:");
+                 
+                        if (int.TryParse(Console.ReadLine(), out numOfChild) && numOfChild >= 0)
+                        {
+                            tempObject[i].NumberOfChildren[numMonth - 1] = numOfChild;
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Please enter a valid value.");
+                        }
+
+                    }
+
+                    Console.Write("Would you like to enter another month (y/n)?\n");
+                    if (Console.ReadLine().ToLower() == "y")
+                    {
+                        continue;
+                    }
+                    break;
+                }
+            }
+               
+            return tempObject;
+        }
+    }
+
+        
+}
+    
+
+
